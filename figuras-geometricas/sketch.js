3edad71b76748076d35b20383a194b8d0d118b8a
Luis Reinoso <lomejordejr@riseup.net>
2018-02-24 14:29:31 -0500

    Agregado figuras geometricas

--- figuras-geometricas/sketch.js
+++ figuras-geometricas/sketch.js
@@ -5,5 +5,29 @@ function setup() {
 }
 
 function draw() {
+	// cambiar color de fondo
 	background(0, 255, 0);
+
+	// rectangulo
+	rect(100, 100, 50, 50, 10, 10, 10,10);
+
+	// elipse
+	ellipse(200, 100, 50, 50);
+
+	// linea
+	line(10, 20, 25, 35);
+
+	// arco
+	arc(300, 300, 200, 100, 5, 10);
+
+	// punto
+	point(30, 30);
+
+	// cuadrilatero
+	fill(25, 100, 200, 255);
+	quad(38, 31, 86, 20, 69, 63, 30, 76);
+
+	// triangulo
+	fill(102, 25, 155, 255);
+	triangle(240, 275, 258, 220, 286, 275);
 }
